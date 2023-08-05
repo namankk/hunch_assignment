e9756edbe07671bf656f9b8b10287246ffd84342
namankashyap <naman.k@purplle.com>
2023-08-05 13:05:20 +0530

    - Unused widget removed - text aligment issue resolved

--- lib/main.dart
+++ lib/main.dart
@@ -11,68 +11,45 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: ListView.separated(
-          itemCount: 5,
-          itemBuilder: (context, index) {
-            return Stack(
-              children: [
-                Container(
-                  margin: const EdgeInsets.all(15),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.rectangle,
-                      border: Border.all(color: Colors.black, width: 1)),
-                  child: Column(
-                    children: [centerButton(context), bottomMenu()],
-                  ),
-                ),
-                closeButtonWidget()
-              ],
-            );
-          },
-          separatorBuilder: (context, index) {
-            return const SizedBox(
-              height: 10,
-            );
-          }), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: Scaffold(
+          appBar: AppBar(
+            // TRY THIS: Try changing the color here to a specific color (to
+            // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
+            // change color while the other colors stay the same.
+            backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            // Here we take the value from the MyHomePage object that was created by
+            // the App.build method, and use it to set our appbar title.
+            title: const Text("Hunch Assignment"),
+          ),
+          body: ListView.separated(
+              itemCount: 5,
+              itemBuilder: (context, index) {
+                return Stack(
+                  children: [
+                    Container(
+                      margin: const EdgeInsets.all(15),
+                      decoration: BoxDecoration(
+                          shape: BoxShape.rectangle,
+                          border: Border.all(color: Colors.black, width: 1)),
+                      child: Column(
+                        children: [centerButton(context), bottomMenu()],
+                      ),
+                    ),
+                    closeButtonWidget()
+                  ],
+                );
+              },
+              separatorBuilder: (context, index) {
+                return const SizedBox(
+                  height: 10,
+                );
+              }), // This trailing comma makes auto-formatting nicer for build methods.
+        ));
   }
 
   SizedBox centerButton(BuildContext context) {
@@ -129,7 +106,7 @@ class _MyHomePageState extends State<MyHomePage> {
                   ),
                   Container(
                     padding: const EdgeInsets.only(left: 2.0, right: 15.0),
-                    margin: const EdgeInsets.only(right: 15.0),
+                    margin: const EdgeInsets.only(left: 15.0, right: 15.0),
                     decoration: const BoxDecoration(
                         shape: BoxShape.rectangle, color: Colors.amber),
                     child: const Text("coming soon",
